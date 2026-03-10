0f03140c76147efcd21b4fb9521e222115104ee1
lipoka <lipokadam98@gmail.com>
2026-03-10 14:17:48 +0100

    Initialize Spring Boot project with basic structure and configuration, including Maven Wrapper, dependencies, and `.gitignore`.

---
+++ src/test/java/com/memora/memora_backend/MemoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memora.memora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
