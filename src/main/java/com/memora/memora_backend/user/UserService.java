ceb288a2991771a8012e1a1b3410890fc547058c
lipoka <lipokadam98@gmail.com>
2026-04-16 22:22:24 +0200

    Remove `UserController`, `UserService`, and `UserServiceImpl` along with their associated implementations and mappings.

--- src/main/java/com/memora/memora_backend/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.memora.memora_backend.user;
-
-import java.util.Optional;
-
-public interface UserService {
-    Optional<User> findByEmail(String email);
-    User update(User user);
-    void delete(Long id);
-}
