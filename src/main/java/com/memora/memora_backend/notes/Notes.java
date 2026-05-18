0cf5a6f0991841b566d5dc6356d455ce7236dd5b
lipoka <lipokadam98@gmail.com>
2026-05-18 19:45:53 +0200

    Add `Notes` entity with JPA annotations for persistence and relationships.

---
+++ src/main/java/com/memora/memora_backend/notes/Notes.java
@@ -0,0 +1,41 @@
+package com.memora.memora_backend.notes;
+
+import com.memora.memora_backend.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "notes")
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = Instant.now();
+    }
+}
