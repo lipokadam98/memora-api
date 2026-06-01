ed0555dfc9d64e4645ad76bb7266c69e9ef10b22
lipoka <lipokadam98@gmail.com>
2026-06-01 20:13:41 +0200

    Annotate `NoteControllerTest` with `@WebMvcTest` and set active profile to `dev`

--- src/test/java/com/memora/memora_backend/note/NoteControllerTest.java
+++ src/test/java/com/memora/memora_backend/note/NoteControllerTest.java
@@ -1,4 +1,9 @@
 package com.memora.memora_backend.note;
 
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@WebMvcTest(NoteController.class)
+@ActiveProfiles("dev")
 public class NoteControllerTest {
 }
